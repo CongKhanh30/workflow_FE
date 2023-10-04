e3d853b29cf65ca46703d14754c13729bf472216
Shine <113924792+HeanShine@users.noreply.github.com>
2023-10-04 17:03:38 +0700

    create login FE 10/4-17h:00 bang

--- workflow/src/App.js
+++ workflow/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from "./component/home/home";
+import Login from "./component/login/login";
+import {Route, Routes} from "react-router";
 
 function App() {
   return (
-    <div className="App">
-
-      <Home/>
-    </div>
+      <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/home" element={<Home/>} />
+      </Routes>
   );
 }
 
