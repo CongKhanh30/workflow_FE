ec3941706dc9c1402a7125e23ab4cdc82ecb9a42
CongKhanh30 <khanhnc45@wru.vn>
2023-10-05 10:52:48 +0700

    register - 10:52 - 05/10/23

--- workflow/src/App.js
+++ workflow/src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from "./component/home/home";
+import Home from "./component/home/Home";
+import {Route, Routes} from "react-router";
+import Register from "./component/register/Register";
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
+            <Routes>
+                {/*<Route path={"/login"} element={<Login/>}></Route>*/}
+                <Route path={"/register"} element={<Register/>}></Route>
 
-      <Home/>
-    </div>
-  );
+            </Routes>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ workflow/src/component/register/Register.js
@@ -0,0 +1,128 @@
+import React, {useState} from 'react';
+import Service from "../service/Service.js";
+import {Link} from "react-router-dom";
+import {useNavigate} from 'react-router-dom';
+import {ErrorMessage, Field, Form, Formik} from "formik";
+import * as Yup from "yup";
+
+const Register = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
+    const register = () => {
+        let data = new FormData();
+        let name = document.getElementById("name").value;
+        let username = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+
+        data.append('name', name);
+        data.append('username', username);
+        data.append('password', password);
+
+
+        Service.register(data).then((response) => {
+            alert("Đăng ký thành công !")
+            console.log(response)
+            setIsLoggedIn(true);
+            // setIsLoggedIn(true);
+            navigate('/login');
+        }).catch((error) => {
+            console.log(error)
+            // console.log(error)
+            alert("Đăng ký thất bại !")
+        })
+    }
+
+    const validateSchema = Yup.object().shape({
+        name: Yup.string()
+            .min(6, "Tên người dùng có ít nhất 6 ký tự!")
+            .required("Tên người dùng không được để trống"),
+        username: Yup.string()
+            .min(6, "Tên tài khoản có ít nhất 6 ký tự!")
+            .required("Tên tài khoản không được để trống"),
+        password: Yup.string()
+            .min(6, "Mật khẩu có ít nhất 6 ký tự!")
+            .required("Mật khẩu không được để trống"),
+        cpassword: Yup.string()
+            .min(6, "Mật khẩu có ít nhất 6 ký tự!")
+            .required("Mật khẩu không được để trống")
+    });
+
+    return (
+        <div>
+            <div className="container d-flex align-items-center justify-content-center" style={{minHeight: "100vh"}}>
+                <div className="d-flex flex-column justify-content-between">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-6 col-xl-5 col-md-10 ">
+                            <div className="card card-default mb-0">
+                                <div className="card-header pb-0">
+                                    <div className="app-brand w-100 d-flex justify-content-center border-bottom-0">
+                                        <a className="w-auto pl-0" href="/index.html">
+                                            <img src="images/logo.png" alt="Mono"/>
+                                            <span className="brand-name text-dark">MONO</span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="card-body px-5 pb-5 pt-0">
+                                    <h4 className="text-dark text-center mb-5">Sign Up</h4>
+                                    <Formik initialValues={{
+                                        name: '',
+                                        username: '',
+                                        password: '',
+                                        cpassword: ''
+                                    }}
+                                            validationSchema={validateSchema}
+                                            onSubmit={(values) => {
+                                        console.log(values);
+                                    }}>
+                                        <Form>
+                                            <div className="row">
+                                                <div className="form-group col-md-12 mb-4">
+                                                    <Field type="text" className="form-control input-lg" id="name"
+                                                           name={'name'} aria-describedby="nameHelp" required="required"
+                                                           placeholder="Name"/>
+                                                    <ErrorMessage name="name"  component="div" className="text-danger" />
+                                                </div>
+                                                <div className="form-group col-md-12 mb-4">
+                                                    <Field type="text" className="form-control input-lg" id="username"
+                                                           name={'username'} placeholder="Username"/>
+                                                    <ErrorMessage name="username"  component="div" className="text-danger" />
+                                                </div>
+                                                <div className="form-group col-md-12 ">
+                                                    <Field type="password" className="form-control input-lg"
+                                                           id="password" name={'password'} placeholder="Password"/>
+                                                    <ErrorMessage name="password"  component="div" className="text-danger" />
+                                                </div>
+                                                <div className="form-group col-md-12 ">
+                                                    <Field type="password" className="form-control input-lg"
+                                                           id="cpassword" name={'cpassword'}
+                                                           placeholder="Confirm Password"/>
+                                                    <ErrorMessage name="cpassword"  component="div" className="text-danger" />
+                                                </div>
+                                                <div className="col-md-12">
+
+                                                    <button type="submit" className="btn btn-primary btn-pill mb-4"
+                                                            onClick={() => register()}>Sign Up
+                                                    </button>
+
+                                                    <p>Already have an account?
+                                                        <Link to={"/login"}>
+                                                            <a className="text-blue">Sign in</a>
+                                                        </Link>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </Form>
+                                    </Formik>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Register;
