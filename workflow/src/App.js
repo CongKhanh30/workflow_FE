ea2c4d403aa4437a16e18b3e6565d602f0d862fe
Ry4c <131599009+Ry4c@users.noreply.github.com>
2023-10-06 15:44:48 +0700

    predone drag 15:44/06/10/23

--- workflow/src/App.js
+++ workflow/src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import Home from "./component/home/home";
 import Login from "./component/login/login";
 import {Route, Routes} from "react-router";
+import Board from "./component/board";
+import {Provider} from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-      <Routes>
-          <Route path="/" element={<Login/>} />
-          <Route path="/home" element={<Home/>} />
-      </Routes>
+      <Provider store={store}>
+          <Routes>
+              {/*<Route path="/" element={<Login/>} />*/}
+              {/*<Route path="/home" element={<Home/>} />*/}
+              <Route path={'/'} element={<Board/>}/>
+          </Routes>
+      </Provider>
+
   );
 }
 
