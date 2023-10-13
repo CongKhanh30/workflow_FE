5011e8f1c0cd91fa268831d209bcb7c31a5f1e6d
Shine <113924792+HeanShine@users.noreply.github.com>
2023-10-13 09:18:15 +0700

    push removeBoard and EditBoard demo past 2 9:18-13/10 bang

--- workflow/src/App.js
+++ workflow/src/App.js
@@ -9,7 +9,6 @@ import HomeTeams from "./component/home/HomeTeams";
 import Login from "./component/login/Login";
 import CreateHomeTeams from "./component/home/CreateHomeTeams";
 import CreateBoard from "./board/CreateBoard";
-import EditBoard from "./board/EditBoard";
 
 function App() {
     return (
