f39374eca1ebe7da4c8fcbc5e67060b6e0b62917
CongKhanh30 <khanhnc45@wru.vn>
2023-10-17 10:41:27 +0700

    update giao dien trang chu - 10:41 - 17/10/23

--- workflow/src/component/home/HomeTeams.js
+++ workflow/src/component/home/HomeTeams.js
@@ -67,40 +67,107 @@ const HomeTeams = () => {
         <>
             <style>
                 {`
-          body {
+           body {
             font-family: Arial, sans-serif;
-            background-color: #f7f7f7;
             margin: 0;
             padding: 0;
-          }
-          header {
-            background-color: #333;
-            color: #fff;
-            text-align: center;
-            padding: 10px 0;
-          }
-          h2 {
-            margin: 0;
-          }
-          .container {
-            max-width: 1200px;
-            margin: 0 auto;
-            padding: 20px;
+            background-color: #F9F9F9;
+            }
+        
+        .head {
+            font-weight: bold;
+        }
+        p {
+            color: black;
+        }
+        p:hover{
+        color: #5279cb;
+        }
+        h1 {
             display: flex;
-            flex-wrap: wrap;
-            justify-content: space-between; 
-          }
-          .product {
-            background-color: #fff;
-            border: 5px solid black;
+            justify-content: center;
+            align-items: center;
+            font-size: 40px;
+            color: black;
+        }
+        #sidebar {
+            width: 250px;
+            height: 100%;
+            background: #CDDAF3;
+            position: fixed;
+        }
+
+        #sidebar ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        #sidebar ul li {
+            padding: 10px;
+        }
+
+        #sidebar ul li a {
+            color: black;
+            text-decoration: none;
+        }
+
+        #content {
+            margin-left: 250px;
             padding: 20px;
-            width: calc(20% - 20px); 
-            margin-bottom: 20px;
-            border-radius: 15%;
-            position: relative;
-            left: 250px;
+        }
+
+        #content h1 {
+            font-size: 24px;
+        }
+
+        .group-list { 
+            margin-top: 20px;
+            max-height: 350px; /* Chiều cao tối đa cho danh sách nhóm */
+            overflow-y: auto; /* Thanh cuộn dọc */
+        }
+        ::-webkit-scrollbar-track {
+            background: #f1f1f1;
+        }   
+        ::-webkit-scrollbar-thumb {
+        background: #84abfa; 
+        }
+        ::-webkit-scrollbar-thumb:hover {
+          background: #5279cb; 
+        }
+        .group-item {
+            display: flex;
+            margin-bottom: 10px;
+            border: 1px solid #ddd;
+            padding: 10px;
+            background-color: #CDDAF3;
+        }
+
+        .group-item a {
             
-           }
+            text-decoration: none;
+            color: #333;
+        }
+
+        .group-item {
+            margin-top: 10px;
+        }
+        
+        .group-actions {
+            width : 50%;
+        }
+        
+        member-list {
+            width : 50%;
+            margin-top: 10px;
+            border: 1px solid #ddd;
+            padding: 10px;
+            background-color: #f9f9f9;
+        }
+
+        .member-list a {
+            text-decoration: none;
+            color: #333;
+        }
         `}
             </style>
 
@@ -185,76 +252,61 @@ const HomeTeams = () => {
 
 
                 <div>
-                    <div className="cotainer">
-                        <h1 className="head">List Team</h1>
-
-                        <Link to={"/createTeam"}>
-                            <button className="btn btn-primary">Add Team</button>
-                        </Link>
-
-
-                        <table className="table table-striped">
-                            <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>name</th>
-                                <th>danh sach thanh vien</th>
-
-                                <th colSpan="2" className="action">Action</th>
-
-                            </tr>
-                            </thead>
-
-                            <tbody>
-
-                            {listTeam.map((team, index) => {
-                                    return (
-
-                                        <tr key={index}>
-                                            <td>{team.id}</td>
-                                            <td>{team.name}</td>
-
-                                            <td>{team.members.map((member, index) => {
-                                                return (
-                                                    <div key={index}>
-                                                        <p>{member}</p>
+                    <div>
+                        <div className="container">
+                            <h1 className="head">DANH SÁCH NHÓM</h1>
+                            <div id="sidebar">
+                                <ul>
+                                    <li><a href="#">Đổi thông tin cá nhân</a></li>
+                                    <Link to={"/changePassword/:id"}>
+                                        <li><p>Đổi mật khẩu</p></li>
+                                    </Link>
+                                </ul>
+                            </div>
+                            <div id="content">
+                                <Link to={"/createTeam"}>
+                                    <button className="btn btn-info">Thêm Nhóm Mới</button>
+                                </Link>
+                                <div style={{height: "80vh", overflow: "auto", marginTop: "15px"}}>
+                                    {listTeam.map((team, index) => {
+                                            return (
+                                                <div className="group-list">
+                                                    <div className="group-item">
+                                                        <div className="group-actions">
+                                                            <Link to={"/b/" + team.id}>
+                                                                <span style={{fontSize: "1.2rem", paddingLeft:"30px"}}>{team.name}</span><br/>
+                                                            </Link>
+                                                            <button className="btn btn-pill btn-danger" style={{ height:"2rem", padding: "0.4rem", fontSize: "0.8rem"}}
+                                                                    onClick={() => removeTeam(team.id)}>Delete
+                                                            </button>
+                                                            <span> </span>
+                                                            | <button className="btn btn-pill btn-smoke" style={{ height:"2rem", padding: "0.4rem", fontSize: "0.8rem"}} data-toggle="modal"
+                                                                      data-target="#modalAddMember"
+                                                                      onClick={() => {
+                                                                          addMember(team.id)
+                                                                      }
+                                                                      }>
+                                                            Add Member
+                                                        </button>
+                                                        </div>
+                                                        <div className="member-list">
+                                                            <p style={{fontSize: "1.2rem"}}>Danh sách thành viên: </p>
+                                                            <p>{team.members.map((member, index) => {
+                                                                return (
+                                                                    <span key={index}>
+                                                                {member}<span> </span>
+                                                            </span>
+                                                                )
+                                                            })}</p>
+                                                        </div>
                                                     </div>
-                                                )
-                                            })}</td>
-
-                                            <td>
-                                                <Link to={"/b/" + team.id}>
-                                                    <button className="btn btn-warning">Detail</button>
-                                                </Link>
-                                            </td>
-
-                                            <td>
-                                                <button className="btn btn-danger"
-                                                        onClick={() => removeTeam(team.id)}>delete
-                                                </button>
-                                            </td>
-
-                                            <td>
-
-                                                <button type="button" className="btn btn-primary" data-toggle="modal"
-                                                        data-target="#modalAddMember"
-                                                        onClick={() => {
-                                                            addMember(team.id)
-                                                        }
-                                                        }>
-                                                    Them Nguoi
-                                                </button>
-
-                                            </td>
-                                        </tr>
-
-
-                                    )
-                                }
-                            )}
-
-                            </tbody>
-                        </table>
+                                                </div>
+                                            )
+                                        }
+                                    )}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </>
